c6cfffe97a3efe387a2a4449378a1094d3ee1f3e
Ancel Puthoor <ancelputhoor88@gmail.com>
2025-06-23 21:00:10 +0530

    hangman

--- Hangman.java
+++ Hangman.java
@@ -7,62 +7,115 @@ public class Hangman {
         String word = "fool";
         int wrongGuess = 0;
         String guess;
-        int charIndex;
-        ArrayList<Character> wordState ;
+
+        ArrayList<Character> wordState;
         ArrayList<Integer> indexes;
-        int maxWrongGuess=5;
+        int maxWrongGuess = 6;
 
         try (Scanner scanner = new Scanner(System.in)) {
 
-            wordState= new ArrayList<>();
+            wordState = new ArrayList<>();
 
             for (int i = 0; i < word.length(); i++) {
                 wordState.add('_');
             }
 
-            while (wrongGuess < maxWrongGuess && wordState.contains('_')) {
-                System.out.print("Enter your guess");
-                guess = scanner.next();
+            System.out.println("****************");
+            System.out.println("Welcome to HangMan");
+            System.out.println("****************");
 
-               if (word.contains(guess)) {
-                    indexes=findIndexes(word, guess);
+            printWordState(wordState);
+            while (wrongGuess < maxWrongGuess && wordState.contains('_')) {
+                System.out.println(hangMan(wrongGuess));
+                System.out.print("Enter your guess:");
+                guess = scanner.next().toLowerCase();
+
+                if (word.contains(guess)) {
+                    indexes = findIndexes(word, guess);
                     for (Integer index : indexes) {
-                       
+
                         wordState.set(index, guess.charAt(0));
                     }
-               }else{
-                wrongGuess++;
-               }
+                } else {
+                    System.out.println("wrong guess");
+                    wrongGuess++;
+                }
 
-                System.out.println(wordState);
+                printWordState(wordState);
             }
             if (wrongGuess == maxWrongGuess || wordState.contains('_')) {
-                System.out.println(" you failed");
+                System.out.println(hangMan(wrongGuess));
+                System.out.println(" YOU DIED");
+                System.out.println("Correct word: "+word);
+
+            } else {
+                System.out.println("YOU WON");
             }
-            else{
-                System.out.println("you won");
-            }
-           
 
         } catch (Exception e) {
 
         }
     }
-
+    static void printWordState(ArrayList<Character> wordList){
+        System.out.print("Word: ");
+            for (Character character : wordList) {
+                System.out.print(character +" ");
+            }
+            System.out.println();
+    }
     static ArrayList<Integer> findIndexes(String word, String guess) {
 
         ArrayList<Integer> index = new ArrayList<>();
         int searchIndex = 0;
         while (true) {
-            searchIndex=word.indexOf(guess, searchIndex);
-            if (searchIndex ==-1) {
+            searchIndex = word.indexOf(guess, searchIndex);
+            if (searchIndex == -1) {
                 break;
             }
             index.add(searchIndex);
             searchIndex++;
-            
+
         }
 
         return index;
     }
+
+    static String hangMan(int wrongGuess) {
+        return switch (wrongGuess) {
+            case 0 -> """
+
+
+                    """;
+            case 1 -> """
+                             o
+
+                    """;
+            case 2 -> """
+                             o
+                             |
+                    """;
+            case 3 -> """
+                             o
+                            /|
+
+                    """;
+            case 4 -> """
+                             o
+                            /|\\
+
+                    """;
+            case 5 -> """
+                             o
+                            /|\\
+                            /
+                    """;
+            case 6 -> """
+                             o
+                            /|\\
+                            / \\
+                    """;
+            default -> "";
+        };
+    }
+
 }
